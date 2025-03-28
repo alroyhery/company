8a64cb284122d8014c25ab1c6f985fd223fe7f00
imamrovid9 <imamrovid9@gmail.com>
2025-03-28 13:54:31 +0700

    update

--- resources/views/foot.blade.php
+++ resources/views/foot.blade.php
@@ -1,61 +1,79 @@
 <footer class="footer">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-10">
-                <h2>Lets Join Digis, </br>We Protect Your Money</h1>
-                    <div class="row pt-4">
-                        <div class="col-md-6 d-flex">
-                            <p>
-                                <i class="mdi mdi-google-maps text-primary mr-2"
-                                    style="border-radius: 50%; padding: 7px; background-color: #fff"></i>
-                            </p>
-                            <p>
-                                Kemang Raya No.4, RT.11/RW.1, Bangka, Kec.
-                                Mampang Prpt.,
-                                Kota Jakarta
-                                Selatan,
-                                Daerah
-                                Khusus Ibukota Jakarta 12730
+    <div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-10">
+                    <h2>Lets Join Digis, </br>We Protect Your Money</h1>
+                        <div class="row pt-4">
+                            <div class="col-md-6 d-flex">
+                                <p>
+                                    <i class="mdi mdi-google-maps text-primary mr-2"
+                                        style="border-radius: 50%; padding: 7px; background-color: #fff"></i>
+                                </p>
+                                <p>
+                                    Kemang Raya No.4, RT.11/RW.1, Bangka, Kec.
+                                    Mampang Prpt.,
+                                    Kota Jakarta
+                                    Selatan,
+                                    Daerah
+                                    Khusus Ibukota Jakarta 12730
 
-                            </p>
+                                </p>
+                            </div>
+                            <div class="col-md-6 d-flex">
+                                <p>
+                                    <i class="mdi mdi-email text-primary mr-2"
+                                        style="border-radius: 50%; padding: 8px; background-color: #fff"></i>
+                                </p>
+                                <p>
+                                    <a href="mailto:digis@gmail.com" class="text-white">digis@gmail.com</a>
+                                </p>
+                            </div>
                         </div>
-                        <div class="col-md-6 d-flex">
-                            <p>
-                                <i class="mdi mdi-email text-primary mr-2"
-                                    style="border-radius: 50%; padding: 8px; background-color: #fff"></i>
-                            </p>
-                            <p>
-                                <a href="mailto:digis@gmail.com" class="text-white">digis@gmail.com</a>
-                            </p>
-                        </div>
-                    </div>
+                </div>
+
+
+
             </div>
+            <hr style="background-color: #3B8CF4; height: 1px; border: 0;" />
+
+
+            <div class="d-md-flex justify-content-between">
+                <div>
+                    <p>&copy; Digital Inti Generasi</p>
+                </div>
+                <div class="d-md-flex d-sm-block">
+                    <ul class="list-unstyled d-md-flex">
+                        <li>
+                            <a href="#" class="text-white mr-2">Terms & Condition</a>
+                        </li>
+                        <li>
+                            <a href="#" class="text-white">Privacy Policy</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+
 
 
 
         </div>
-        <hr style="background-color: #3B8CF4; height: 1px; border: 0;" />
-
-
-        <div class="d-md-flex justify-content-between">
-            <div>
-                <p>&copy; Digital Inti Generasi</p>
-            </div>
-            <div class="d-md-flex d-sm-block">
-                <ul class="list-unstyled d-md-flex">
-                    <li>
-                        <a href="#" class="text-white mr-2">Terms & Condition</a>
-                    </li>
-                    <li>
-                        <a href="#" class="text-white">Privacy Policy</a>
-                    </li>
-                </ul>
+        <div class="absolute"
+            style="width: 300px; height:400px; float: right; overflow:hidden; 
+            margin-top:-400px; background-color:aquamarine">
+            <div class="relative">
+                <div class="absolute">
+                    <span
+                        style="font-weight: bold; float: right; font-size:30em; overflow:hidden; margin-right:-80px; opacity:0.1">
+                        C
+                    </span>
+                </div>
             </div>
         </div>
-
-
-
     </div>
+
+
 
     {{-- <div
         style="width:600px; height:600px; overflow:hidden; background-color:black; margin-top: -500px; position:absolute; float:left;">
